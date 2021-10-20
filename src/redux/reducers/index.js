1278a816daec6238e3d1140a903d5915d1f990a8
qasim.habib <qasim.habib26@gmail.com>
2021-10-20 23:48:29 +0500

    Initial commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import { productReducer, selectProduct } from "./productReducer";
+
+const reducers= combineReducers({
+    allProducts : productReducer,
+    product : selectProduct
+})
+
+export default reducers
